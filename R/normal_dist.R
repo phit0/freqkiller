35ba60e7376e994ea8eedbdc845959990098146b
Philipp Schad <philipp.schad@stud.uni-goettingen.de>
2019-06-16 17:34:32 +0200

    functions freed from global variables

--- R/normal_dist.R
+++ R/normal_dist.R
@@ -6,29 +6,31 @@ h <- function(eta) {
 
 #first derivative of h
 dh <- function(eta) {
-  out <- 1
+  out <- rep(1, dim(eta)[1])
   return(out)
 }
 
 #sigma function
-sigma_vec <- function(beta_t) {
+sigma_vec <- function(y) {
   out <- rep(1, length(y))
   return(out)
 }
 
 #beta prior
 prior <- function(beta_t){
-  out <- dmvnorm(beta_t, mean = m(beta_t), sigma = M(beta_t), log = T)
+  out <- mvtnorm::dmvnorm(beta_t, mean = m(beta_t), sigma = M(beta_t), log = T)
   return(out)
 }
 
 #likelihood for beta
-like_beta <- function(beta_t) {
-  out <- dmvnorm(y, mean = X%*%beta_t , sigma = diag(length(y)),log=T)
+like_beta <- function(beta_t, X, y) {
+  mean = X%*%beta_t
+  sigma = diag(length(y))
+  out <- dmvnorm(y, mean, sigma, log=T)
   return(out)
 }
 
 #posterior for beta
-post_beta <- function(beta_t){
-  return(like_beta(beta_t) + prior(beta_t))
+post_beta <- function(beta_t, X, y){
+  return(like_beta(beta_t, X, y) + prior(beta_t))
 }
