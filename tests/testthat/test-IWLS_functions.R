890e8cbc2e8f938af48092f299fe7e99bc60bebb
Philipp Schad <philipp.schad@stud.uni-goettingen.de>
2019-08-20 08:55:16 +0200

    test1

--- tests/testthat/test-IWLS_functions.R
+++ tests/testthat/test-IWLS_functions.R
@@ -1,23 +1,21 @@
 context("")
 
-test_that("Covariance matrix M of prior for beta a squared Matrix", {
-  b <- c(2, 3)
-  expect_equal(dim(M(b))[1], dim(M(b))[2])
-})
-
-test_that("Covariance matrix M of prior for beta is an invertible Matrix", {
-  b <- c(2, 3)
-  D <- M(b)
-  expect_equal((D%*%solve(D)),  diag(ncol(D))) #inverse*D is identity
-  expect_false(round(det(D), 10e12) == 0) #nonzero determinant
-  expect_equal(qr(D)$rank, ncol(D)) #full rank matrix
-})
-
 
 # test_that("sigma_vec is positive",{
 #   expect_true(sigma_vec(c(0, 0)) >= 0)
 #   expect_true(sigma_vec(0) >= 0)
 # })
+rnorm(4)
 
-
+test_that("w_func works", {
+t1 <- sum(w_func(sigma2_t = 1, beta_t =  c(1,2), y = c(-0.9, 0.7, -0.35, -0.85),
+                 X = cbind(1, c(1, 2, 3, 4)), dist = "normal"))
+expect_equal(t1, 4)
+t2 <- sum(w_func(sigma2_t = 1, beta_t =  c(1,2), y = c(1, 0, 0, 1),
+           X = cbind(1, c(1, 2, 3, 4)), dist = "bernoulli"))
+expect_equal(t2, 0.05285832)
+t3 <- sum(diag(w_func(sigma2_t = 1, beta_t =  c(1,2), y = c(0, 1, 0, 0),
+                 X = cbind(1, c(1, 2, 3, 1.5)), dist = "poisson")))
+expect_equal(t3, 1319.73)
+})
 
